33b3d446eecc952709d1cde15fbd91470f03e9b4
handipriyono <handimacbook@gmail.com>
2018-05-06 15:03:20 +0700

    fix board error not sync

--- boggle.js
+++ boggle.js
@@ -1,11 +1,41 @@
 class Boggle {
-  constructor() {
+  constructor(num) {
     //let kamus = require('./data.js');
 
     //using my own dictionary
-    let kamus = ['MAKAN', 'HACKTIV', 'ES', 'SEGAR', 'BLACK', 'BLUE', 'NASI', 'lAPAR', 'MINUM', 'HAUS', 'WAKTU', 'BERLALU', 'CEPAT', 'JAVA', 'LARI', 'DUDUK', 'PUASA', 'LAGU', 'MENYANYI', 'HORE', 'WOW', 'BUAH', 'RIP', 'GREEN', 'TOS', 'SOLVE', 'REST', 'TART']
+    let kamus = [
+      'MAKAN',
+      'HACKTIV',
+      'ES',
+      'SEGAR',
+      'BLACK',
+      'BLUE',
+      'NASI',
+      'lAPAR',
+      'MINUM',
+      'HAUS',
+      'WAKTU',
+      'BERLALU',
+      'CEPAT',
+      'JAVA',
+      'LARI',
+      'DUDUK',
+      'PUASA',
+      'LAGU',
+      'MENYANYI',
+      'HORE',
+      'WOW',
+      'BUAH',
+      'RIP',
+      'GREEN',
+      'TOS',
+      'SOLVE',
+      'REST',
+      'TART',
+    ];
     this.kamus = kamus;
-
+    this.num = num || 4;
+    this.board = this.makeBoard(this.num);
   }
 
   isiAcak() {
@@ -26,11 +56,10 @@ class Boggle {
     return arr;
   }
 
-
   //Cek arah kanan kebawah && diagonal
-  cekkanan(manyboards) {
-    let board = this.makeBoard(manyboards)
-
+  cekkanan() {
+    let board = this.board;
+    var ss = [];
     var count = 0;
     var kamus1 = 0;
     var temp = [];
@@ -41,11 +70,13 @@ class Boggle {
     for (let i = 0; i < kamus.length; i++) {
       kamus1 = 0;
       temp = [];
+      ss = [];
       for (let j = 0; j < board.length; j++) {
         for (let k = 0; k < board[j].length; k++) {
           if (kamus[i][kamus1] === board[j][k]) {
             kamus1++;
             temp.push(board[j][k]);
+            ss.push(j + '' + k);
 
             if (temp.join('') === kamus[i]) {
               rightResult.push(kamus[i]);
@@ -56,30 +87,31 @@ class Boggle {
         }
       }
     }
-    return rightResult
+    return rightResult;
   }
 
-
-
   //cek kekiri && keatas diagonal
-  cekkiri(manyboards) {
-    let board = this.makeBoard(manyboards)
+  cekkiri() {
+    let board = this.board;
 
+    var cek = [];
     var count = 0;
     var kamus1 = 0;
     var temp = [];
     var leftResult = [];
-
+    ss = [];
     var kamus = this.kamus;
 
     for (let i = kamus.length - 1; i >= 0; i--) {
       kamus1 = 0;
       temp = [];
+      var ss = [];
       for (let j = board.length - 1; j >= 0; j--) {
         for (let k = board[j].length - 1; k >= 0; k--) {
           if (kamus[i][kamus1] === board[j][k]) {
             kamus1++;
             temp.push(board[j][k]);
+            ss.push(j + ' ' + k);
 
             if (temp.join('') === kamus[i]) {
               leftResult.push(kamus[i]);
@@ -90,42 +122,32 @@ class Boggle {
         }
       }
     }
-    return leftResult
+    return leftResult;
   }
 
   cekdiagonalAcak() {
     //WOW
-
   }
 
-
-
-
-  Show(no) {
-    let board = this.makeBoard(no)
-    console.log(board)
-    let left = Bogglegame.cekkiri(no)
-    let right = Bogglegame.cekkanan(no)
-    var combine = (left.concat(right))
-    let final = []
+  Show() {
+    let board = this.board;
+    console.log(board);
+    let left = Bogglegame.cekkiri();
+    let right = Bogglegame.cekkanan();
+    var combine = left.concat(right);
+    let final = [];
 
     for (let i = 0; i < combine.length; i++) {
       if (final.includes(combine[i]) === false) {
-        final.push(combine[i])
+        final.push(combine[i]);
       }
-
     }
 
-
-
-
-    console.log('_________________________________')
-    return final.length + ' Words found: ' + '\n' + final.join(' \n')
-
-
+    return final.length + ' Words found: ' + '\n' + final.join(' \n');
   }
 }
 //end of class
-var Bogglegame = new Boggle();
+
 //input how many board will be shown below:
-console.log(Bogglegame.Show(8));
+var Bogglegame = new Boggle(7);
+console.log(Bogglegame.Show());
